111da61cea8c3502c27b75d363b949bc6c0c197d
Asya Tealdi <asya2698@live.it>
2024-05-18 20:53:37 +0200

    #25 - Publishers and Subscribers pt.1

---
+++ SwiftUIIntermediateLearning/SubscriberBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  SubscriberBootcamp.swift
+//  SwiftUIIntermediateLearning
+//
+//  Created by Asya Tealdi on 17/05/2024.
+//
+
+import SwiftUI
+import Combine
+
+class SubscriberViewModel: ObservableObject {
+    @Published var count: Int = 0
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setUpTimer()
+    }
+    
+    func setUpTimer() {
+        Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                self.count += 1
+                
+            }
+            .store(in: &cancellables)
+    }
+}
+struct SubscriberBootcamp: View {
+    @StateObject var vm = SubscriberViewModel()
+    var body: some View {
+        VStack {
+            Text("\(vm.count)")
+                .font(.largeTitle)
+        }
+    }
+}
+
+#Preview {
+    SubscriberBootcamp()
+}
