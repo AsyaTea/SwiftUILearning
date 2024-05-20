15df214724e24795c02d8ab27b06c0ed2933c43a
Asya Tealdi <asya2698@live.it>
2024-05-20 13:45:51 +0200

    #25 - Publishers and Subscribers pt.2

--- SwiftUIIntermediateLearning/SubscriberBootcamp.swift
+++ SwiftUIIntermediateLearning/SubscriberBootcamp.swift
@@ -12,10 +12,32 @@ class SubscriberViewModel: ObservableObject {
     @Published var count: Int = 0
     @Published var textFieldText = ""
     
+    @Published var textIsValid = false
+    @Published var showButton = false
+    
     var cancellables = Set<AnyCancellable>()
     
     init() {
         setUpTimer()
+        addTextFieldSubscriber()
+        addButtonSubscriber()
+    }
+    
+    func addTextFieldSubscriber() {
+        $textFieldText
+        //debounce does the function after 0.5 second the user finished typing
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map { text -> Bool in
+                if text.count > 3 {
+                    return true
+                }
+                return false
+            }
+//            .assign(to: \.textIsValid, on: self)
+            .sink(receiveValue: { [weak self] isValid in
+                self?.textIsValid = isValid
+            })
+            .store(in: &cancellables)
     }
     
     func setUpTimer() {
@@ -29,17 +51,68 @@ class SubscriberViewModel: ObservableObject {
             }
             .store(in: &cancellables)
     }
+    
+    func addButtonSubscriber() {
+        $textIsValid
+            .combineLatest($count)
+            .sink { [weak self] isValid, count in
+                guard let self = self else { return }
+                if isValid && count >= 10 {
+                    self.showButton = true
+                } else {
+                    self.showButton = false
+                }
+            }
+            .store(in: &cancellables)
+    }
 }
 struct SubscriberBootcamp: View {
     @StateObject var vm = SubscriberViewModel()
+    let bgColor = Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1))
+
     var body: some View {
         VStack {
             Text("\(vm.count)")
                 .font(.largeTitle)
             
-            TextField("Type something..", text: vm.$textFieldText)
+            Text(vm.textIsValid.description)
+            
+            TextField("Type something..", text: $vm.textFieldText)
+                .padding(.leading)
                 .frame(height: 55)
+                .font(.headline)
+                .background(bgColor)
+                .cornerRadius(10)
+                .overlay(alignment: .trailing) {
+                    ZStack {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.red)
+                            .opacity(
+                                vm.textFieldText.count < 1 ? 0.0 :
+                                    vm.textIsValid ? 0.0 : 1.0)
+                        
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                            .opacity(vm.textIsValid ? 1.0 : 0.0)
+                    }
+                    .font(.title)
+                    .padding(.trailing)
+                }
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Submit")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .opacity(vm.showButton ? 1.0 : 0.5)
+                    
+            })
+            .disabled(!vm.showButton)
         }
+        .padding()
     }
 }
 
