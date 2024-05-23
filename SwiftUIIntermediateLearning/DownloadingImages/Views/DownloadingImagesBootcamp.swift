45ee2f18960ce86a6a5a005ae4d5dfa4e5f019a7
Asya Tealdi <asya2698@live.it>
2024-05-23 21:57:16 +0200

    #28 - Downloading images pt.1

---
+++ SwiftUIIntermediateLearning/DownloadingImages/Views/DownloadingImagesBootcamp.swift
@@ -0,0 +1,22 @@
+//
+//  DownloadingImagesBootcamp.swift
+//  SwiftUIIntermediateLearning
+//
+//  Created by Asya Tealdi on 23/05/2024.
+//
+
+import SwiftUI
+
+struct DownloadingImagesBootcamp: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Hello")
+            }
+        }
+    }
+}
+
+#Preview {
+    DownloadingImagesBootcamp()
+}
